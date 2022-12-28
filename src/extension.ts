4018ea9046f1dafe3e6b8da88f8871358f0cbe92
Eduardo Vitor <eduardovitor730@gmail.com>
2022-12-28 11:13:53 -0300

    Say falando os warnings

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,24 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	let readWarnings = vscode.commands.registerCommand('warningsreader.readWarnings', () => {
-		// const translate = require("translate");
-		// let trans_text = await translate(infos_string, "pt");
+	let readWarnings = vscode.commands.registerCommand('warningsreader.readWarnings', async () => {
+		const translate = require("translate");
 		const say = require("say");
-		// translate.engine = "google";
-		// translate.key = process.env.GOOGLE_KEY;
+		translate.engine = "google";
+		translate.key = process.env.GOOGLE_KEY;
 		let diagnostics = vscode.languages.getDiagnostics();
 		let warnings = diagnostics[0][1];
-		if (warnings.length!=0){
+		if (warnings.length!=0) {
 			let infos_string = "";
 			warnings.forEach(function (value) {
-				let info=value.message.replace(/["]/g,'');
+				let info = value.message.replace(/["]/g,'');
 				infos_string = infos_string + "   " + info; 
 			}); 
-			say.speak(infos_string);
+			let trans_text = await translate(infos_string, "pt");
+			say.speak(trans_text);
 		}
 		else{
-			say.speak("There are no warnings");
+			say.speak("Não há avisos ou erros")
 		}
 	});
 
